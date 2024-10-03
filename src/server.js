b525cb81ed913dd04c56ad9542cc2f5badea98c3
samuelbrag4 <samuelbraga.tds2.senai@gmail.com>
2024-10-03 09:01:39 -0300

    refactor: alter server to use routes

--- src/server.js
+++ src/server.js
@@ -1,17 +1,20 @@
-import express from "express";
-import { config } from "dotenv";
+// Imports - Express e Dotenv
+import express  from 'express';
+import { config } from 'dotenv';
 
+// Configuração do ambiente de desenvolvimento
 config();
+import routes from './routes/index.routes.js';
 
+// Porta do servidor - 3000 se não achar
 const serverPort = process.env.PORT || 3000;
 
+// Express - Iniciando o servidor
 const app = express();
 app.use(express.json());
+app.use(routes);
 
-app.get("/", (req, res) => {
-  return res.status(200).json({ message: "Hello, World!" });
-});
-
+// Servidor - Iniciando o servidor
 app.listen(serverPort, () => {
-  console.log(`⚡ Server started on http://localhost:${serverPort}`);
+    console.log(`👨‍🎤 - Server started on http://localhost:${serverPort} - 👨‍🎤`);
 });
